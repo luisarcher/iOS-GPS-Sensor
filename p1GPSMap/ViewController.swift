7e47ceba44e558446ab16a08901cabbe271091ea
luisarcher <a21201026@isec.pt>
2017-01-04 18:47:52 +0000

    iOS GPS Sensor

--- p1GPSMap/ViewController.swift
+++ p1GPSMap/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CLLocationManagerDelegate{
 
+    @IBOutlet weak var mapa: MKMapView!
+    @IBOutlet weak var lock: UISwitch!
+    
+    let lm = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        lm.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            lm.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            lm.delegate = self
+            lm.startUpdatingLocation()
+        }
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !lock.isOn{
+            let local = locations.last!.coordinate
+            mapa.camera = MKMapCamera(lookingAtCenter: local, fromDistance: 500, pitch: 0, heading: 0)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //mapa.zoo
     }
 
+    @IBAction func onDeis(_ sender: Any) {
+        let coordDeis = CLLocationCoordinate2D(latitude: 40.1924, longitude: -8.4128)
+        let regiaoDEIS = MKCoordinateRegion(center: coordDeis, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001))
+        
+        mapa.setRegion(regiaoDEIS, animated: true)
+        
+        let DEIS = MKPointAnnotation()
+        
+        DEIS.coordinate = coordDeis
+        DEIS.title = "DEIS-ISEC"
+        
+        mapa.addAnnotation(DEIS)
+    }
 
 }
 
